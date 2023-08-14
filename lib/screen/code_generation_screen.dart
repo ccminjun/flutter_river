1181bae83a2f541bbdd12e259420f543d0a1119f
Minjun Choi <mi478@naver.com>
2023-08-14 22:28:24 +0900

    provider 공부

--- lib/screen/code_generation_screen.dart
+++ lib/screen/code_generation_screen.dart
@@ -9,7 +9,9 @@ class CodeGenerationScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext, WidgetRef ref) {
-    
+
+    print('build');
+
     final state1 = ref.watch(gStateProvider);
     final state2 = ref.watch(gStateFutureProvider);
     final state3 = ref.watch(gStateFuture2Provider);
@@ -17,7 +19,7 @@ class CodeGenerationScreen extends ConsumerWidget {
       number1:10,
       number2:20,
     ));
-    final state5 = ref.watch(gStateNotifierProvider);
+
 
     return DefaultLayout(
         title: 'CodeGenerationScreen',
@@ -51,7 +53,20 @@ class CodeGenerationScreen extends ConsumerWidget {
               loading: () => Center(child: CircularProgressIndicator()),
             ),
             Text('State4 : $state4'),
-            Text('State5 : $state5'),
+            // _StateFiveWidget(),
+            Consumer(
+                builder: (context, ref, child){
+                  final state5 = ref.watch(gStateNotifierProvider);
+
+                  return Row(
+                    children: [
+                      Text('State5: $state5'),
+                      if(child != null) child,
+                    ],
+                  );
+                },
+              child: Text('hello'),
+            ),
             Row(
               children: [
                 ElevatedButton(
@@ -86,5 +101,16 @@ class CodeGenerationScreen extends ConsumerWidget {
           ],
         ),
     );
+  }
+}
+
+class _StateFiveWidget extends ConsumerWidget {
+  const _StateFiveWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state5 = ref.watch(gStateNotifierProvider);
+
+    return Text('State5 : $state5');
   }
 }
